b5817716f94e88c8a5c3f26520142484a6a5ea8c
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-10-06 01:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+            var select = document.getElementById("colorSelect");
+            var selectedOption = select.options[select.selectedIndex];
+            
+            if (selectedOption) {
+                select.removeChild(selectedOption);
+            }
+        }
